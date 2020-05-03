f6fed2e67363074f82fa8e5a333fbf1d12328908
David Adams <david.riding.rails@gmail.com>
2020-05-03 13:02:39 -0700

    add sortable method to application helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def sortable(column, title=nil, query=nil)
+    title ||= column.titleize
+    direction = column == sort_column && sort_direction == "asc" ? "desc" : "asc"
+    link_to title, { sort: column, direction: direction, query: query }
+  end  
 end
