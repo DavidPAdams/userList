2963521f80939915c52bdcc78e5a7c3ab8c4f979
David Adams <david.riding.rails@gmail.com>
2020-05-03 12:00:39 -0700

    update config to webpack envoronment for bootstrap

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,14 @@
 const { environment } = require('@rails/webpacker')
 
+const webpack = require('webpack')
+
+environment.plugins.prepend('Provide',
+  new webpack.ProvidePlugin({
+    $: 'jquery',
+    JQuery: 'jquery',
+    jquery: 'jquery',
+    Popper: ['popper.js', 'default']
+  })
+)
+
 module.exports = environment
