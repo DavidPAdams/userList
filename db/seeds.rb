6279ea240caa5115a477dd1253e7747697221e39
David Adams <david.riding.rails@gmail.com>
2020-05-03 12:09:56 -0700

    update seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+1000.times do |x|
+  firstName = Faker::Name.first_name
+  lastName = Faker::Name.last_name
+  title = Faker::Name.prefix
+  phone = Faker::PhoneNumber.cell_phone
+  email = Faker::Internet.safe_email(name: "#{firstName}#{x}")
+  name = "#{lastName}, #{firstName}"
+  status = x%4>0 ? true : false
+  User.create!(name: name,
+               email: email,
+               title: title,
+               phone: phone,
+               status: status,
+               created_at: Time.zone.now,
+               updated_at: Time.zone.now)
+end
